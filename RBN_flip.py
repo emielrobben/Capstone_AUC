28941e4a6c0438640b8d02b1066ff19eb6b62823
emielrobben@gmail.com <emielrobben@gmail.com>
2023-04-25 12:43:02 +0200

    New version based on other concept of r

--- RBN_flip.py
+++ RBN_flip.py
@@ -1,10 +1,9 @@
-#Markov
-# first the normal model: 
 import networkx as nx
 import matplotlib.pyplot as plt
 import random
 import numpy as np
 from scipy.sparse import coo_matrix
+
 
 class RBN:
     def __init__(self, K, N, flip_probability):
@@ -14,43 +13,40 @@ class RBN:
         self.G = nx.DiGraph()
         self.initialization()
         self.generate_logic_tables()
-        
+
     def initialization(self):
         expected_degrees = [self.K for _ in range(self.N)]
-        # Create a random graph with the specified expected degrees
         self.G = nx.expected_degree_graph(expected_degrees, selfloops=False)
         self.G = nx.DiGraph(self.G)
         for u, v in list(self.G.edges()):
-            # Randomly reverse some edges with 50% probability
             if random.random() < 0.5:
                 self.G.remove_edge(u, v)
                 self.G.add_edge(v, u)
-        # Initialize the node states with random boolean values      
         for i in range(self.N):
             self.G.nodes[i]["state"] = random.choice([True, False])
-        
+
     def generate_logic_tables(self):
         for i in self.G.nodes:
             inputs = list(self.G.predecessors(i))
-            # Generate random truth tables for each node
-            truth_table = [random.choice([True, False]) for i in range(2**len(inputs))]
+            truth_table = [random.choices([True, False], weights=[self.flip_probability, 1 - self.flip_probability])[0]
+                           for _ in range(2 ** len(inputs))]
             self.G.nodes[i]["inputs"] = inputs
             self.G.nodes[i]["truth_table"] = truth_table
-            
+
     def bin_to_dec(self, bin_list):
         dec = 0
         # Convert binary list to decimal number
         for i in range(len(bin_list)):
             if bin_list[i]:
-                dec = dec + 2**(len(bin_list) - 1 - i)
+                dec = dec + 2 ** (len(bin_list) - 1 - i)
         return dec
-    
+
     def probabilistic_flip(self, value):
         # Flip the value with the given probability
         if random.random() < self.flip_probability:
             return not value
         return value
-            
+
     def step(self, show_plot=False):
         new_state = []
         for node in self.G.nodes:
@@ -77,7 +73,7 @@ class RBN:
         colors = [color_map[self.G.nodes[node]["state"]] for node in self.G.nodes()]
         nx.draw(self.G, with_labels=True, node_color=colors, font_weight='bold')
         plt.show()
-        
+
     def _generate_transitions(self):
         for current_state in range(2 ** self.N):
             for updated_node in range(self.N):
@@ -87,15 +83,16 @@ class RBN:
                     index = self.bin_to_dec(input_vals)
                     output = self.G.nodes[updated_node]["truth_table"][index]
                     if output == updated_state:
-                        next_state = current_state ^ ((current_state >> updated_node) & 1 ^ updated_state) << updated_node
+                        next_state = current_state ^ (
+                                    (current_state >> updated_node) & 1 ^ updated_state) << updated_node
                         probability = (1 - self.flip_probability) if output else self.flip_probability
                         yield current_state, next_state, probability
-    
+
     def create_initial_vector_and_sparse_matrix(self):
         # Step 1: Create initial probability vector
         num_states = 2 ** self.N
         init_prob_vector = np.full(num_states, 1 / num_states)
-        
+
         # Step 2: Create sparse matrix in COO format
         row = []
         col = []
@@ -110,24 +107,25 @@ class RBN:
         transition_matrix = coo_matrix((data, (row, col)), shape=(num_states, num_states))
 
         return init_prob_vector, transition_matrix
-    
+
     def find_stationary_distribution(self, transition_matrix, initial_vector, num_iterations=100):
         # Create a copy of the initial probability vector to avoid modifying the original vector
         current_vector = initial_vector.copy()
-    
+
         # Perform the power iteration method for the given number of iterations
         for _ in range(num_iterations):
             # Multiply the current vector by the transition matrix
             current_vector = transition_matrix.dot(current_vector)
-    
+
         # Normalize the resulting vector to ensure it is a probability distribution
         stationary_distribution = current_vector / np.sum(current_vector)
-    
+
         # Return the stationary distribution
         return stationary_distribution
 
+
 # Create an instance of the RBN class with 5 inputs per node, 40 nodes, and flip probability of 0.4
-k_av= 4
+k_av = 4
 N = 20
 flip_prob = 0.0
 network = RBN(k_av, N, flip_prob)
